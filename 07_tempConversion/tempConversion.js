201c33fd5e9aa836ba50f0c1e3e06ac3ee21ebd4
nikoskar93 <skarpanikoleta@gmail.com>
2022-07-15 19:01:39 +0300

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcious=((fahrenheit-32)*5/9);
+  return Math.round(celcious*10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+ let f=c *9/5 +32;
+ return Math.round(f*10) /10;
 };
 
 // Do not edit below this line
