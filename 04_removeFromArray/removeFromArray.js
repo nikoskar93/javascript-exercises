7068c5080795447a8d24cddf45cea0598af26a39
nikoskar93 <skarpanikoleta@gmail.com>
2022-07-15 18:38:44 +0300

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let myArray=args[0];
+    
+    return myArray.filter(val => !args.includes(val))
+        
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    
+    let sumOfAll=0;
+    let maxNumber= (Math.max(num1, num2));
+    let minNumber= (Math.min(num1, num2));
+    if (num1<0 || num2<0) {
+        return ('ERROR');
+    } else if (typeof(num1) !== 'number'|| typeof(num2)!=='number') {
+        return ('ERROR');
+    } else {
+        for (let i=minNumber; i<=maxNumber; i++ ) {
+            sumOfAll+=i;
+        }
+        return sumOfAll;
+    } };
 
 // Do not edit below this line
 module.exports = sumAll;
